f6fe618677cc454e7a27039618640820a23e4626
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-06-26 20:36:48 +0200

    changed: refactoring

---
+++ src/middlewares/date.validation.middleware.js
@@ -0,0 +1,25 @@
+
+const moment = require('moment')
+
+exports.hasValidDate = (req, res, next) => {
+  const _date = req.params.date
+  const date1 = moment(_date, 'MM-DD-YY').format('MM/DD/YY')
+  const date2 = new Date(_date)
+
+  if (!_date) {
+    return res.status(412).json({ error: 'Missing date field' })
+  }
+
+  let dd = date2.getDate()
+  let mm = date2.getMonth() + 1
+
+  const yy = date2.getFullYear().toString().substr(-2)
+  if (dd < 10) dd = '0' + dd
+  if (mm < 10) mm = '0' + mm
+
+  if (date1 !== `${mm}/${dd}/${yy}`) {
+    return res.status(409).json({ error: 'Invalid date format' })
+  }
+
+  return next()
+}
--- src/middlewares/user.validation.middleware.js
+++ src/middlewares/user.validation.middleware.js
@@ -3,9 +3,8 @@ exports.hasFirstnameAndLastname = (req, res, next) => {
   const _firstname = req.query.firstname
   const _lastname = req.query.lastname
 
-  if (!_firstname || !_lastname) {
-    return res.status(412).json({ error: 'Missing firstname and/or lastname fields' })
-  }
+  if (!_firstname) return res.status(412).json({ error: 'Missing firstname field' })
+  if (!_lastname) return res.status(412).json({ error: 'Missing lastname field' })
 
   return next()
 }
