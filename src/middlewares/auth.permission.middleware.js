b63d25bea5e73a20390d79ad74488ead06a6bdb9
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-07-01 21:03:45 +0200

    fixed: provided a base to parseInt()

--- src/middlewares/auth.permission.middleware.js
+++ src/middlewares/auth.permission.middleware.js
@@ -1,6 +1,6 @@
 exports.minimumPermissionLevelRequired = (requiredPermissionLevel) => {
   return (req, res, next) => {
-    const userPermissionLevel = parseInt(req.jwt.permissionLevel)
+    const userPermissionLevel = parseInt(req.jwt.permissionLevel, 10)
     if (userPermissionLevel && requiredPermissionLevel) {
       return next()
     } else {
