e3b66bb1b9268b2de835e93d008c96b4301b33f6
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-07-01 20:53:27 +0200

    fixed: untracked files

--- src/middlewares/cours.validation.middleware.js
+++
@@ -1,7 +0,0 @@
-exports.hasCoursField = (req, res, next) => {
-  const _cours = req.params.cours
-
-  if (!_cours) return res.status(412).json({ error: 'Missing cours name field' })
-
-  return next()
-}
