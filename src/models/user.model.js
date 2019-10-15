4b6319c9649295bea3fcaa60caa0cbcfb8ef1d85
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-10-15 14:09:32 +0200

    feat(user-model): updated user-model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,9 +8,12 @@ const userSchema = new Schema({
   password: { type: String, minLength: 6, maxLength: 64, required: true },
   permissionLevel: { type: String, enum: ['MEMBER', 'ADMIN'], default: 'MEMBER', required: true },
   grade: { type: String, enum: ['B1', 'B2', 'B3', 'I1', 'I2'], required: true },
+  group: { type: String, enum: ['G1', 'G2', 'G3', 'G1 (dev)', 'G2 (dev)', 'G3 (dev)', 'G1 (infra-réseau)', 'G2 (infra-réseau)', 'G3 (infra-réseau)'], required: true },
   city: { type: String, enum: ['Arras', 'Auxerre', 'Bordeaux', 'Brest', 'Grenoble', 'Lille', 'Lyon', 'Montpellier', 'Nantes', 'Paris', 'Dakar'], required: true },
   bts: { type: Boolean, default: false, required: false },
-  isActive: { type: Boolean, default: false, required: true }
+  isActive: { type: Boolean, default: false, required: true },
+  hasInformationMails: { type: Boolean, default: true, required: true },
+  creationDate: { type: String, required: true, default: () => Date.now() }
 })
 
 module.exports = mongoose.model('User', userSchema)
