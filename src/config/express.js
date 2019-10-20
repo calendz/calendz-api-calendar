b9718f23d8de4f8a0889eb4ad2414c14655df741
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-10-20 20:36:20 +0200

    refactor(express): disabled logs for "/v1/health-check"

--- src/config/express.js
+++ src/config/express.js
@@ -18,7 +18,10 @@ const app = express()
 /* istanbul ignore if */
 // logging method
 if (config.node_env === 'development') {
-  app.use(morgan('dev', { stream: { write: message => logger.info(message.trim()) } }))
+  app.use(morgan('dev', {
+    stream: { write: message => logger.info(message.trim()) },
+    skip: (req, res) => req.originalUrl === '/v1/health-check'
+  }))
 } else if (config.node_env === 'production') {
   /* istanbul ignore next */
   app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }))
