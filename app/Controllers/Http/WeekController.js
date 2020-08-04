7a2ab3964c691be31b34873730e3140f5fa734f9
Arthur Dufour <dufourarthur.perso@gmail.com>
2020-08-04 16:08:11 +0200

    feat(cache): added parameter to ignore cache

--- app/Controllers/Http/WeekController.js
+++ app/Controllers/Http/WeekController.js
@@ -12,14 +12,16 @@ class WeekController {
    * Get courses of the current week
    */
   async getCurrent ({ request }) {
-    const { firstname, lastname } = request.only(['firstname', 'lastname'])
+    const { firstname, lastname, ignoreCache } = request.only(['firstname', 'lastname', 'ignoreCache'])
 
     // get current date and format with moment
     const date = moment(new Date()).format('MM/DD/YYYY')
 
     // try to retrieve and return data from cache/redis
-    const data = await Cache.getWeek(firstname, lastname, date)
-    if (data) return data
+    if (!ignoreCache) {
+      const data = await Cache.getWeek(firstname, lastname, date)
+      if (data) return data
+    }
 
     // if not cached, scrap it
     const result = await Scrapper.fetchWeek(firstname, lastname, date)
@@ -38,15 +40,17 @@ class WeekController {
    * Get courses of a given week
    */
   async getByDate ({ request, params }) {
-    const { firstname, lastname } = request.only(['firstname', 'lastname'])
+    const { firstname, lastname, ignoreCache } = request.only(['firstname', 'lastname', 'ignoreCache'])
 
     // check if date is valid, if yes format it
     if (!DateUtils.isValid(params.date)) throw new InvalidDateException()
     const date = moment(params.date, 'MM-DD-YYYY').format('MM/DD/YY')
 
     // try to retrieve and return data from cache/redis
-    const data = await Cache.getWeek(firstname, lastname, date)
-    if (data) return data
+    if (!ignoreCache) {
+      const data = await Cache.getWeek(firstname, lastname, date)
+      if (data) return data
+    }
 
     // if not cached, scrap it
     const result = await Scrapper.fetchWeek(firstname, lastname, date)
