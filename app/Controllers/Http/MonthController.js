8dbf4be133dc1612936141b1acd6bdb835b8074a
Arthur Dufour <dufourarthur.perso@gmail.com>
2020-10-07 21:14:35 +0200

    refactor(month-controller): rewrite getCurrent() (fixed many issues)

--- app/Controllers/Http/MonthController.js
+++ app/Controllers/Http/MonthController.js
@@ -5,7 +5,7 @@ const Cache = use('Cache')
 const Scrapper = use('Scrapper')
 const ScrappingException = use('App/Exceptions/ScrappingException')
 
-const numberOfWeekByMonth = 4
+const numberOfWeekByMonth = 5
 
 class MonthController {
   /**
@@ -17,27 +17,36 @@ class MonthController {
     const result = []
 
     for (let i = 0; i < numberOfWeekByMonth; i++) {
-      // get current date and format with moment
-      const date = moment(new Date()).add(i, 'M').format('MM/DD/YYYY')
+      let cacheUsed = false
+      // get current date (+ i * 7 days) and format with moment
+      const date = moment(new Date()).add(i * 7, 'd').format('MM/DD/YY')
 
-      // try to retrieve and return data from cache/redis
+      // ---------------------------------------------------
+      // Use cache (or at least, try to)
+
       if (!ignoreCache) {
+      // Get data from cache
         const data = await Cache.getWeek(firstname, lastname, date)
         if (data) {
+          cacheUsed = true
           result.push(data)
-        } else {
-          // if not cached, scrap it
-          const data = await Scrapper.fetchWeek(firstname, lastname, date)
-            .catch(() => {
-            /* istanbul ignore next */
-              throw new ScrappingException()
-            })
-
-          result.push(data)
-
-          // set scrap result in cache
-          await Cache.setWeek(firstname, lastname, date, data)
         }
+      }
+
+      // ---------------------------------------------------
+      // Data not in cache: scrapping
+
+      if (ignoreCache || !cacheUsed) {
+        // if not cached, scrap it
+        const data = await Scrapper.fetchWeek(firstname, lastname, date)
+          .catch(() => { /* istanbul ignore next */ throw new ScrappingException() })
+
+        result.push(data)
+
+        // set scrap result in cache
+        await Cache.setWeek(firstname, lastname, date, data)
+        // indicate to not restart scrapping today
+        await Cache.setIsDailyScrapped(firstname, lastname, date)
       }
     }
 
